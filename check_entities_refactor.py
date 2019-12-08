be702fecf3d8df1b84ab5be4bcd6c45dc38834be
DESKTOP-JP-HOME\Tebbe <jptebbe@gmail.com>
2019-12-08 21:01:02 +0100

    no message

--- check_entities_refactor.py
+++ check_entities_refactor.py
@@ -19,8 +19,10 @@ for filepath in glob.glob(os.path.join(folder_path, "*.csv")):
         for row in reader:
             # delete empty list elements & filter out already seen elements
             new_row = [x for x in row if x and x not in found_values]
+
             # update marker set with row contents
             found_values.update(row)
+            print(found_values)
             if new_row:
-                # new row isn't empty: write it
+                #new row isn't empty: write it
                 writer.writerow(new_row)
